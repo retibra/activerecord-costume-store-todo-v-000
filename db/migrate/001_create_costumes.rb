2911a06627a01109f0fb3343cc890673e828515b
Timothy Ege <retibra@gmail.com>
2018-07-08 21:42:51 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name
       t.integer :price
-      t.string :url
+      t.string :image_url
       t.string :size
       t.datetime :created_at
       t.datetime :updated_at
